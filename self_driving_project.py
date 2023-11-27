8048b9bb8aa5a6aa186fada7e6a9d6a6132f7c6f
davidoises-wpi <dmoises@wpi.edu>
2023-11-27 21:35:56 +0000

    Plotting global route to be passed to local planner

--- self_driving_project.py
+++ self_driving_project.py
@@ -1,5 +1,8 @@
 import carla
 import random
+from scout.navigation.global_route_planner import GlobalRoutePlanner
+import live_plotter as lv
+import time
 
 def main():
     client = carla.Client('localhost', 2000)
@@ -9,6 +12,7 @@ def main():
     traffic_manager.set_synchronous_mode(True)
 
     world = client.get_world()
+    map = world.get_map()
 
     settings = world.get_settings()
     settings.synchronous_mode = True
@@ -19,12 +23,68 @@ def main():
     bp = random.choice(blueprint_library.filter('vehicle.tesla.*'))
 
     # init_pos = carla.Transform(carla.Location(x=158.0, y=24.0, z=0.05), carla.Rotation(yaw=-90))
-    spawn_point = random.choice(world.get_map().get_spawn_points())
-    vehicle = world.spawn_actor(bp, spawn_point)
-    print(spawn_point)
+    # spawn_point = random.choice(world.get_map().get_spawn_points())
+    spawn_point = carla.Transform(carla.Location(x=26.382587, y=-57.401386, z=0.6), carla.Rotation(yaw=-0.023438))
+    # This is the player
+    vehicle = world.try_spawn_actor(bp, spawn_point)
 
-    destination_point = random.choice(world.get_map().get_spawn_points())
-    print(destination_point)
+    # destination_point = random.choice(world.get_map().get_spawn_points())
+    destination_point = carla.Transform(carla.Location(x=-45.149696, y=55.715389, z=0.600000), carla.Rotation(yaw=-90.161217))
+
+    sampling_resolution = 2.0
+    global_route_plannner = GlobalRoutePlanner(map, sampling_resolution)
+
+    start_waypoint = map.get_waypoint(spawn_point.location)
+    end_waypoint = map.get_waypoint(destination_point.location)
+
+    start_location = start_waypoint.transform.location
+    print("Start location: ", start_location)
+    end_location = end_waypoint.transform.location
+    print("End location: ", end_location)
+    # Returns list of (carla.Waypoint, RoadOption)
+    # The carla.Waypoint class contains a carla.Transform object
+    # The transform object contains a location and a rotation
+    route_trace = global_route_plannner.trace_route(start_location, end_location)
+    print(len(route_trace))
+    x_points = []
+    y_points = []
+    for i in range(len(route_trace)):
+        x_points.append(route_trace[i][0].transform.location.x)
+        y_points.append(route_trace[i][0].transform.location.y)
+
+    lp_traj = lv.LivePlotter(tk_title="Trajectory Trace")
+
+    FIGSIZE_X_INCHES   = 8      # x figure size of feedback in inches
+    FIGSIZE_Y_INCHES   = 8      # y figure size of feedback in inches
+    PLOT_LEFT          = 0.1    # in fractions of figure width and height
+    PLOT_BOT           = 0.1    
+    PLOT_WIDTH         = 0.8
+    PLOT_HEIGHT        = 0.8
+
+    ###
+    # Add 2D position / trajectory plot
+    ###
+    trajectory_fig = lp_traj.plot_new_dynamic_2d_figure(
+            title='Vehicle Trajectory',
+            figsize=(FIGSIZE_X_INCHES, FIGSIZE_Y_INCHES),
+            edgecolor="black",
+            rect=[PLOT_LEFT, PLOT_BOT, PLOT_WIDTH, PLOT_HEIGHT])
+    
+    trajectory_fig.set_invert_x_axis() # Because UE4 uses left-handed 
+                                           # coordinate system the X
+                                           # axis in the graph is flipped
+    trajectory_fig.set_axis_equal()    # X-Y spacing should be equal in size    
+
+    # Add waypoint markers
+    trajectory_fig.add_graph("waypoints", window_size=len(x_points),
+                                x0=x_points, y0=y_points,
+                                linestyle="-", marker="", color='g')
+    
+    lp_traj.refresh()
+
+    # CONTROLLER_OUTPUT_FOLDER
+    time.sleep(10)
+    
 
 if __name__ == '__main__':
     main()
